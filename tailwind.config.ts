cf968f7116c61afa14b3aa6360b111136ffc5607
DavidZapataOh <davidzapata5885@gmail.com>
2025-01-21 12:38:50 -0500

    setup project

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,11 +8,42 @@ export default {
   ],
   theme: {
     extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+      colors:{
+        dark: "#05000c",
+        light: "#f5f5f5",
+        primary: "#B72C7A",
+        secundary: "#531354",
+        thirty: "#191538",
       },
+      animation: {
+        'spin-slow': 'spin 8s linear infinite',
+      },
+      backgroundImage:{
+        circularLight: 'repeating-radial-gradient(#f5f5f5 1px, #05000c 8px, #05000c 120px);',
+        circularLightLg: 'repeating-radial-gradient(#f5f5f5 1px, #05000c 8px, #05000c 90px);',
+        circularLightMd: 'repeating-radial-gradient(#f5f5f5 1px, #05000c 8px, #05000c 70px);',
+        circularLightSm: 'repeating-radial-gradient(#f5f5f5 1px, #05000c 8px, #05000c 50px);'
+      }
     },
+    screens:{
+      "2xl": { max: "1535px" },
+      // => @media (max-width: 1535px) { ... }
+
+      xl: { max: "1279px" },
+      // => @media (max-width: 1279px) { ... }
+
+      lg: { max: "1023px" },
+      // => @media (max-width: 1023px) { ... }
+
+      md: { max: "767px" },
+      // => @media (max-width: 767px) { ... }
+
+      sm: { max: "639px" },
+      // => @media (max-width: 639px) { ... }
+
+      xs: { max: "479px" },
+      // => @media (max-width: 479px) { ... }
+    }
   },
   plugins: [],
 } satisfies Config;
